823105815e8abf023f24536c7445215597bccb1f
Brandon451 <brandon.saldanha@gmail.com>
2021-09-30 19:45:05 -0700

    Completed homework 1

--- Homework/HW1/Verilog/sfp.v
+++ Homework/HW1/Verilog/sfp.v
@@ -9,12 +9,29 @@ input clk;
 input acc;
 input relu;
 input reset;
-input   signed [bw-1:0] in;
-output  signed [psum_bw-1:0] out;
+input signed [bw-1:0] in;
+output signed [psum_bw-1:0] out;
 
-reg     signed [psum_bw-1:0] psum_q;
-wire    signed [psum_bw-1:0] psum_relu;
+reg [psum_bw-1:0]extend_in;
 
-//...
+reg signed [psum_bw-1:0] psum_q;
+wire signed [psum_bw-1:0] psum_relu;
+
+always @(posedge clk) begin
+    if (reset == 1)
+        psum_q <= 0;
+    else begin
+        if (acc == 1)
+        psum_q <= psum_q + in;
+    end
+        
+end
+
+assign psum_relu = (psum_q>0)? psum_q : 0;
+
+//psum_q = acc? psum_q + In : psum_q;
+//assign psum_relu = (psum_q>0)? psum_q : 0;
+
+assign out = psum_q;
 
 endmodule
