784881e290a3699c3c3f95e3bd6bbf920a8393a0
Brandon451 <brandon.saldanha@gmail.com>
2021-10-03 21:43:37 -0700

    Snapshots added

--- Homework/HW1/Verilog/sfp.v
+++ Homework/HW1/Verilog/sfp.v
@@ -23,14 +23,13 @@ always @(posedge clk) begin
     else begin
         if (acc == 1)
         psum_q <= psum_q + {{(psum_bw-bw){in[bw-1]}}, in};
+        if (relu == 1)
+        psum_q <= (psum_q>0)? psum_q : 0;
     end
         
 end
 
 assign psum_relu = (psum_q>0)? psum_q : 0;
-
-//psum_q = acc? psum_q + In : psum_q;
-//assign psum_relu = (psum_q>0)? psum_q : 0;
 
 assign out = psum_q;
 
